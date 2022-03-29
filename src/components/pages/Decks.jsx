6b49132fbf189755e7b2a7b24ab7cbc6c89479ca
br1anlee <joonmolee3@gmail.com>
2022-03-29 11:27:06 -0700

    working on cards

--- src/components/pages/Decks.jsx
+++ src/components/pages/Decks.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 // import {useState} from 'react'
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 export default function Deck ({category}) {
     const {id} = useParams()
 
@@ -31,32 +32,38 @@ export default function Deck ({category}) {
 =======
 export default function Deck({ category }) {
   const { id } = useParams();
+=======
+export default function Deck ({category}) {
+    const {id} = useParams()
+>>>>>>> bb8fc1f (working on cards)
 
-  const [currentCategory, setCurrentCategory] = useState({});
+    const [currentCategory, setCurrentCategory] = useState({})
 
-  let deckIdx = category.findIndex((object) => {
-    return object._id === id;
-  });
+    let deckIdx = category.findIndex(object => {
+        return object._id === id
+    })
 
+<<<<<<< HEAD
   let showAllDecks;
 >>>>>>> de4e1b8 (getting decks show and logout fixed)
+=======
+    let showAllDecks;
+>>>>>>> bb8fc1f (working on cards)
 
-  if (deckIdx != -1) {
-    showAllDecks = category[deckIdx].decks.map((deck, i) => {
-      return (
-        <li key={`deck-${i}`}>
-          <Link to={`/category/${id}/deck/${deck._id}`} >
-            {deck.deckName}
-          </Link>
-        </li>
-      );
-    });
-  }
+    if (deckIdx != -1) {
+        showAllDecks = category[deckIdx].decks.map((deck,i) => {
+            return <li><Link to={`/category/${id}/deck/${deck._id}`}>{deck.deckName}</Link></li>
+        })
+    }
 
-  return (
-    <>
-      <h1>Decks</h1>
-      <ul>{showAllDecks}</ul>
-    </>
-  );
+    setCurrentCategory(category[deckIdx])
+
+
+    return (
+        <>
+            <h1>Decks</h1>
+           <ul>{showAllDecks}</ul>
+        </>
+    )
+
 }
