3d69f147a9a349110b9792bcf909192b546a0817
br1anlee <joonmolee3@gmail.com>
2022-03-31 13:49:58 -0700

    committing to pull from origin main branch

--- src/components/pages/VisibleCards.jsx
+++ src/components/pages/VisibleCards.jsx
@@ -30,43 +30,6 @@ function VisibleCards({ currentCards, setCurrentCards, cards, newCard }) {
       </div>
     )
   })
-
-
-    const cardsArray = newCard.map((element, index)=>{
-
-        return (
-          <div className='create-card-div'>
-            <label hidden htmlFor="card-question">
-              Card {index}
-            </label>
-            <div>
-              <input
-                className="create-question-input"
-                required
-                type="text"
-                placeholder="Question.."
-                id="card-question"
-                name="question"
-                value={element.question}
-
-              />
-              <input
-                required
-                className="create-answer-input"
-                type="text"
-                placeholder="Answer.."
-                id="card-answer"
-                name="answer"
-                value={element.answer}
-                        
-              />
-            </div>
-          </div>
-        );
-
-        
-    })
-
     return ( 
             <>
             {cardsArray}
