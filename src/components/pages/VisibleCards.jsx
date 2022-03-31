8400d01cab391df64635e4a7d305d5c16b6d230e
br1anlee <joonmolee3@gmail.com>
2022-03-31 13:45:24 -0700

    commiting to do pull from main branch

--- src/components/pages/VisibleCards.jsx
+++ src/components/pages/VisibleCards.jsx
@@ -31,8 +31,47 @@ function VisibleCards({ currentCards, setCurrentCards, cards, newCard }) {
     )
   })
 
-  return <>{cardsArray}</>
 
+    const cardsArray = newCard.map((element, index)=>{
+
+        return (
+          <div className='create-card-div'>
+            <label hidden htmlFor="card-question">
+              Card {index}
+            </label>
+            <div>
+              <input
+                className="create-question-input"
+                required
+                type="text"
+                placeholder="Question.."
+                id="card-question"
+                name="question"
+                value={element.question}
+
+              />
+              <input
+                required
+                className="create-answer-input"
+                type="text"
+                placeholder="Answer.."
+                id="card-answer"
+                name="answer"
+                value={element.answer}
+                        
+              />
+            </div>
+          </div>
+        );
+
+        
+    })
+
+    return ( 
+            <>
+            {cardsArray}
+            </>
+     );
 }
 
 export default VisibleCards
