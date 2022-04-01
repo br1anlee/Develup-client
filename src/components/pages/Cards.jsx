a8057c7bb1312235db512a29eff6aac05aea0fae
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-03-31 18:12:08 -0700

    update

--- src/components/pages/Cards.jsx
+++ src/components/pages/Cards.jsx
@@ -4,109 +4,151 @@ import EditDeck from "./EditDeck"
 import axios from "axios"
 
 export default function Cards({ category, setCategory, currentUser }) {
-  const { id, deckId } = useParams()
+  const { id, deckId } = useParams();
   const [deckData, setDeckData] = useState({
     cards: [],
-  })
-  const [showForm, setShowForm] = useState(false)
-  const [num, setNum] = useState(0)
-  const [flip, setFlip] = useState(false)
+  });
+  const [showForm, setShowForm] = useState(false);
+  const [num, setNum] = useState(0);
 
-  let navigate = useNavigate()
+  const [flip, setFlip] = useState(false);
+
+  let navigate = useNavigate();
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_SERVER_URL}/api-v1/category/${id}/deck/${deckId}`)
+      .get(
+        `${process.env.REACT_APP_SERVER_URL}/api-v1/category/${id}/deck/${deckId}`
+      )
       .then((response) => {
         // console.log(response.data)
-        setDeckData(response.data)
+        setDeckData(response.data);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [showForm])
+        console.log(err);
+      });
+  }, [showForm]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     axios
-      .delete(`${process.env.REACT_APP_SERVER_URL}/api-v1/category/${id}/deck/${deckId}`, category)
+      .delete(
+        `${process.env.REACT_APP_SERVER_URL}/api-v1/category/${id}/deck/${deckId}`,
+        category
+      )
       .then((response) => {
         // console.log(response.data)
-        return axios.get(process.env.REACT_APP_SERVER_URL + "/api-v1/category")
+        return axios.get(process.env.REACT_APP_SERVER_URL + '/api-v1/category');
       })
       .then((response) => {
-        setCategory(response.data)
-        navigate("/category")
+        setCategory(response.data);
+        navigate('/category');
       })
-      .catch(console.log)
-  }
+      .catch(console.log);
+  };
 
-  // if (!deckData) {
-  //   return <Navigate to="/category" />
-  // }
+  if (!deckData) {
+    return <Navigate to="/category" />;
+  }
+  let showAllCards = deckData.cards.map((card, i) => {
+    return (
+      <div key={`card-${i}`}>
+        <p>Question: {card.question}</p>
+        <p>Answer: {card.answer}</p>
+      </div>
+    );
+  });
 
   const toggleDisplay = () => {
-    setFlip(!flip)
-  }
+    setFlip(!flip);
+  };
 
   const handleAddNum = () => {
-    setFlip(false)
+    setFlip(false);
     if (num >= deckData.cards.length - 1) {
-      setNum(0)
+      setNum(0);
     } else {
-      setNum(num + 1)
+      setNum(num + 1);
     }
-  }
+  };
+  console.log(deckData.cards[num]);
 
   return (
-      <div className="card-master-container">
+    <div className="card-master-container">
+      <div style={{ position: 'relative' }} className="card-container">
+        <h1
+          style={{
+            position: 'absolute',
+            left: '20px',
+            top: '20px',
+            color: 'white',
+            textShadow:
+              '0 0 2px black, 0 0 2px black, 0 0 2px black, 0 0 2px black',
+          }}
+        >
+          {deckData.deckName}
+        </h1>
 
-        {showForm ? (
-          <EditDeck
+        <div className="card-question-container">
+          <p>
+            {deckData.cards[num] === undefined
+              ? 'This is empty'
+              : deckData.cards[num].question}
+          </p>
+        </div>
+
+        {flip ? (
+          <div className="card-answer-container">
+            <p className="question-text">
+              {deckData.cards[num] === undefined
+                ? 'This is empty'
+                : deckData.cards[num].answer}
+            </p>
+          </div>
+        ) : (
+          <div className="card-answer-ghost"></div>
+        )}
+
+        <div className="button-container">
+          {!flip ? (
+            <button onClick={toggleDisplay}>Show Answer</button>
+          ) : (
+            <button onClick={toggleDisplay}>Hide Answer</button>
+          )}
+          <br></br>
+          <button onClick={handleAddNum}>Next Card</button>
+        </div>
+      </div>
+      {currentUser.id === deckData.author ? (
+        <div className="deck-admin-tools">
+          <p>Deck Author Tools 🛠</p>
+          <button
+            className="edit-button"
+            onClick={() => setShowForm(!showForm)}
+          >
+            {showForm ? 'Return' : 'Edit Deck'}
+          </button>
+          <br></br>
+          <button className="delete-button" onClick={handleSubmit}>
+            Delete Deck
+          </button>{' '}
+        </div>
+      ) : (
+        <></>
+      )}
+
+      {showForm ? (
+        <EditDeck
           categoryId={id}
           decksId={deckId}
           category={category}
           setShowForm={setShowForm}
           showForm={showForm}
           deckData={deckData}
-          />
-
-          ) : (
-            <></>
-            )}
-
-        <br></br>
-
-        <div style={{position: "relative"}}className="card-container">
-
-            <h1 style={{position: "absolute", left:"20px", top: "20px", color: "white", textShadow: "0 0 2px black, 0 0 2px black, 0 0 2px black, 0 0 2px black"  }}>{deckData.deckName}</h1>
-
-
-          <div className="card-question-container">
-            <p>{deckData.cards[num] === undefined  ? "This is empty" : deckData.cards[num].question}</p>
-          </div>
-
-          {flip ?  <div className="card-answer-container"><p className="question-text">{deckData.cards[num] === undefined  ? "This is empty" : deckData.cards[num].answer}</p></div> : <div className="card-answer-ghost"></div>}
-
-
-          <div className="button-container">
-            {!flip ? <button onClick={toggleDisplay}>Show Answer</button> : <button onClick={toggleDisplay}>Hide Answer</button>}
-            <br>
-            </br>
-            <button onClick={handleAddNum}>Next Card</button>
-          </div>
-        </div>
-      {currentUser.id === deckData.author ? (
-        <div className="deck-admin-tools">
-          <p>Deck Author Tools 🛠</p>
-          <button className="edit-button" onClick={() => setShowForm(!showForm)}>{showForm ? "Return" : "Edit Deck"}</button>
-          <br></br>
-          <button className="delete-button" onClick={handleSubmit}>Delete Deck</button>{" "}
-        </div>
+        />
       ) : (
         <></>
-
-        )}
-      </div>
-  )
+      )}
+    </div>
+  );
 }
